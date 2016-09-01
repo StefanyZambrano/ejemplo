1718d0bc8e5b28432695d97d1c51caac66c021c3
StefanyZambrano <StefanyZambrano@users.noreply.github.com>
2016-08-31 22:19:16 -0500

    Update Ejemplorepositorio.java
    tercer cambio

--- ejemplorepositorio/src/ejemplorepositorio/Ejemplorepositorio.java
+++ ejemplorepositorio/src/ejemplorepositorio/Ejemplorepositorio.java
@@ -15,7 +15,10 @@ public class Ejemplorepositorio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("   hola chicos ");
+      
+        string mensaje ="";
+        mensaje= "hola";
+        System.out.println(mensaje);
     }
     
 }
